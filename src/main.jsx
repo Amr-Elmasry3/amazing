fb6262dc5080e06df0b7b57de605eaff9a9bd43c
Amr Elmasry <www.amrelmasry66@gmail.com>
2025-07-20 10:47:16 +0300

    Handle routes2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import store from "./app/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/amazing">
+  <BrowserRouter basename="/amazing/">
     <Provider store={store}>
       <App />
     </Provider>
