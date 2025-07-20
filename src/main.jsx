8f6f5f3a23a54406918f987d1679f53e443fe2cb
Amr Elmasry <www.amrelmasry66@gmail.com>
2025-07-20 10:31:46 +0300

    Handle routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/amazing">
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 );
